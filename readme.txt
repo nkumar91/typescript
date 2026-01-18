83b752b1e0d73776586c3a1d14914aba0fbf6102
Nishant <nknishant91@gmail.com>
2026-01-18 22:43:12 +0530

    cluster changeg

--- readme.txt
+++ readme.txt
@@ -1,3 +1,4 @@
 npm install --production
  // "build": "tsup src/index.ts --format esm --clean",
+ pm2 start dist/worker.js -i max
  // "build": "tsup src/index.ts --format esm --clean",
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-
+import cluster from "cluster";
+import os from 'os';
 import http from 'http';
 import app from './app';
 import dotenv from "dotenv";
@@ -6,15 +7,33 @@ import { connectRedis } from "./config/redis";
 // Register models so they are included in sequelize sync
 import './models/index';
 dotenv.config();
-
+const CPU_COUNT = os.cpus().length;
 const PORT: any = process.env.PORT;
-const server = http.createServer(app);
+
+console.log(CPU_COUNT);
+if (cluster.isPrimary) {
+  console.log(`Primary process ${process.pid}`);
+
+  for (let i = 0; i < CPU_COUNT; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker) => {
+    console.log(`Worker ${worker.process.pid} died, restarting...`);
+    cluster.fork();
+  });
+
+}else
+{
 (async () => {
+    const server = http.createServer(app);
     await connectRedis();
     server.listen(PORT, function () {
         console.log(`Server started at http://localhost:${PORT}`)
     })
 })();
+
+}
 
 
 
